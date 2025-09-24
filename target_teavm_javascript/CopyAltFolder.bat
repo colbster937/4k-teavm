d6676afd4825774ae2d0178766392b6664b5ccc8
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-09-24 13:30:01 -0500

    add easy alt folder script

---
+++ target_teavm_javascript/CopyAltFolder.bat
@@ -0,0 +1,10 @@
+@echo off
+if exist javascript_alt (
+    rmdir /s /q javascript_alt
+)
+mkdir javascript_alt
+copy /y javascript\index_alt2.html javascript_alt\index.html
+copy /y javascript\manifest_alt2.json javascript_alt\manifest_alt2.json
+copy /y javascript\icon.png javascript_alt\icon.png
+copy /y javascript\classes.js javascript_alt\classes.js
+copy /y javascript\Minecraft4k_alt2.html javascript_alt\4k.html
