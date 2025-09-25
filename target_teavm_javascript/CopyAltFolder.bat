aa8bcd9802fc5a3d1c922ef0ca7090af79f0a5cf
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-09-25 07:53:16 -0500

    add alt manifest for windows script

--- target_teavm_javascript/CopyAltFolder.bat
+++ target_teavm_javascript/CopyAltFolder.bat
@@ -6,6 +6,7 @@ mkdir javascript_alt
 copy /y javascript\alt2.html javascript_alt\index.html
 copy /y javascript\index.html javascript_alt\alt1.html
 copy /y javascript\manifest.json javascript_alt\manifest_alt2.json
+copy /y javascript\manifest_alt1.json javascript_alt\manifest.json
 copy /y javascript\icon.png javascript_alt\icon.png
 copy /y javascript\classes.js javascript_alt\classes.js
 copy /y javascript\Minecraft4k_alt2.html javascript_alt\4k.html
