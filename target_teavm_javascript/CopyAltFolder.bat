2d1c1ee4256bf4487f196711f392583322b44407
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-09-25 07:50:05 -0500

    fix alt manifest

--- target_teavm_javascript/CopyAltFolder.bat
+++ target_teavm_javascript/CopyAltFolder.bat
@@ -4,7 +4,8 @@ if exist javascript_alt (
 )
 mkdir javascript_alt
 copy /y javascript\alt2.html javascript_alt\index.html
-copy /y javascript\manifest_alt2.json javascript_alt\manifest_alt2.json
+copy /y javascript\index.html javascript_alt\fullscreen.html
+copy /y javascript\manifest.json javascript_alt\manifest_alt2.json
 copy /y javascript\icon.png javascript_alt\icon.png
 copy /y javascript\classes.js javascript_alt\classes.js
 copy /y javascript\Minecraft4k_alt2.html javascript_alt\4k.html
--- target_teavm_javascript/CopyAltFolder.sh
+++ target_teavm_javascript/CopyAltFolder.sh
@@ -1,7 +1,7 @@
 rm -rf javascript_alt/*
 mkdir -p javascript_alt
 cp javascript/alt2.html javascript_alt/index.html
-cp javascript/manifest_alt2.json javascript_alt/manifest_alt2.json
+cp javascript/manifest.json javascript_alt/manifest_alt2.json
 cp javascript/icon.png javascript_alt/icon.png
 cp javascript/classes.js javascript_alt/classes.js
 cp javascript/Minecraft4k_alt2.html javascript_alt/4k.html
